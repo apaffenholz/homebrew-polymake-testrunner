6311bc582a5274c28d255500344e80f31355ad9c
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2024-08-14 13:35:37 +0200

    polymake version 4.12v8

--- Formula/polymake.rb
+++ Formula/polymake.rb
@@ -314,7 +314,7 @@ class Polymake < Formula
       you should start polymake once with
       "polymake --reconfigure"
       to adjust your local ocnfiguration to the new version.
-      Afterwards you can start polymake via "polymake" in a terminal as usual.
+      Afterwards you can start polymake as usual.
     EOS
   end
 
